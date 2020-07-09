18ecb49ab1fbc2d3ebdf364a05884d7fa33703f2
Lorena Toro <lorenatorog11@gmail.com>
2020-07-09 01:27:35 -0500

    Primeros cambios

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='col1 pt-2'>Super Heroes
+      </div>
+      <div  className='col2 m-0'></div>
+    </div>
+  );
+}
+
+export default App;
